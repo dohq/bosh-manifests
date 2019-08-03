4680f5599711e8dc746387d1291f386cde7b47a0
dohq <dorastone@gmail.com>
2019-08-04 03:38:00 +0900

    [mod] ssh script

--- ssh-concourse.sh
+++ ssh-concourse.sh
@@ -5,4 +5,4 @@ IFS=$'\n\t'
 
 bosh int concourse-creds.yml --path /jumpbox_ssh/private_key > cj.pem
 chmod 600 cj.pem
-ssh -o 'StrictHostKeyChecking=no' -i cj.pem jumpbox@$(terraform output -state=terraform/terraform.tfstate lite_concourse_external_ip)
+ssh -o 'StrictHostKeyChecking=no' -i cj.pem jumpbox@$(terraform output -state=${TFSTATE} lite_concourse_external_ip)
