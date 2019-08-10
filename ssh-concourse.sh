bd00e81ac91e05054a09f077fcedbcabf27efe7c
dohq <dorastone@gmail.com>
2019-08-10 20:55:07 +0900

    [mod] remove ssh

--- ssh-concourse.sh
+++ ssh-concourse.sh
@@ -6,3 +6,4 @@ IFS=$'\n\t'
 bosh int concourse-creds.yml --path /jumpbox_ssh/private_key > cj.pem
 chmod 600 cj.pem
 ssh -o 'StrictHostKeyChecking=no' -i cj.pem jumpbox@$(terraform output -state=${TFSTATE} lite_concourse_external_ip)
+rm -rf cj.pem
