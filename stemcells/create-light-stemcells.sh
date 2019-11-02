3db8cd4d6a40733c2f1cfd1be6027d18957bccba
dohq <dorastone@gmail.com>
2019-11-02 23:38:56 +0900

    [mod] actually not delete havy stemcell

--- stemcells/create-light-stemcells.sh
+++ stemcells/create-light-stemcells.sh
@@ -9,18 +9,17 @@ ver="$(echo $url | cut -d "/" -f 5)"
 filename="$(echo $url | cut -d "/" -f 6)"
 light_stemcell_name="lite-bosh-stemcell-$ver-openstack-kvm-ubuntu-xenial-go_agent.tgz"
 stemcell_id="$(openstack image list -f value| grep $ver | cut -d " " -f 1)"
-tmp_dir=$(mktemp -d)
 
 # trap
-trap "rm -rf $tmp_dir; exit 1" 1 2 3 15
+# trap "rm -rf $tmp_dir; exit 1" 1 2 3 15
 
 # Download new stemcells
 echo "Check alrady light stemcell file"
 
-if [[ ! -f $light_stemcell_name ]]; then
+if [[ ! -f $filename ]]; then
   echo "start $filename Download"
-  curl -fsSL -o $tmp_dir/$filename $url
-  echo "end $filename Download: $tmp_dir/$filename"
+  curl -fsSL -o $filename $url
+  echo "end $filename Download: $filename"
 fi
 
 # create light stemcell
@@ -29,7 +28,7 @@ bosh repack-stemcell --version $ver \
 --empty-image \
 --format openstack-light \
 --cloud-properties="{\"image_id\": \"$stemcell_id\"}" \
-$tmp_dir/$filename ./$light_stemcell_name
+$filename ./$light_stemcell_name
 
 # end
 rm -rf $tmp_dir
