00f4c8aca1ebf40d46f647cea28a28c77485b335
dohq <dorastone@gmail.com>
2019-11-02 21:41:59 +0900

    [add] create light stemcell script

---
+++ stemcells/create-light-stemcells.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+
+# vars
+url=$(bosh int ../bosh-deployment/openstack/cpi.yml --path=/name=stemcell/value/url)
+ver="$(echo $url | cut -d "/" -f 5)"
+filename="$(echo $url | cut -d "/" -f 6)"
+light_stemcell_name="lite-bosh-stemcell-$ver-openstack-kvm-ubuntu-xenial-go_agent.tgz"
+stemcell_id="$(openstack image list -f value| grep $ver | cut -d " " -f 1)"
+tmp_dir=$(mktemp -d)
+
+# trap
+trap "rm -rf $tmp_dir; exit 1" 1 2 3 15
+
+# Download new stemcells
+echo "Check alrady light stemcell file"
+
+if [[ ! -f $light_stemcell_name ]]; then
+  echo "start $filename Download"
+  curl -fsSL -o $tmp_dir/$filename $url
+  echo "end $filename Download: $tmp_dir/$filename"
+fi
+
+# create light stemcell
+echo "Stat create light stemcell"
+bosh repack-stemcell --version $ver \
+--empty-image \
+--format openstack-light \
+--cloud-properties="{\"image_id\": \"$stemcell_id\"}" \
+$tmp_dir/$filename ./$light_stemcell_name
+
+# end
+rm -rf $tmp_dir
+echo "Finished create light stemcell"
