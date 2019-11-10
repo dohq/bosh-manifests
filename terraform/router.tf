09b48a2547fe25e9f72f6fbc05ab6a436e7c59cf
dohq <dorastone@gmail.com>
2019-11-10 22:37:25 +0900

    [mod] lb management network to add router

--- terraform/router.tf
+++ terraform/router.tf
@@ -14,3 +14,8 @@ resource "openstack_networking_router_interface_v2" "external-port" {
   router_id = "${openstack_networking_router_v2.router.id}"
   subnet_id = "${openstack_networking_subnet_v2.internal-subnet.id}"
 }
+
+resource "openstack_networking_router_interface_v2" "lb-mgmt-port" {
+  router_id = "${openstack_networking_router_v2.router.id}"
+  subnet_id = "${openstack_networking_subnet_v2.lb-mgmt-net-subnet.id}"
+}
